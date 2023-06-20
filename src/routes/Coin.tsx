2f69ec04c42495584085d8a515b6465394954d23
minheonjun <tamunavollt1@gmail.com>
2023-06-20 13:50:18 +0900

    react-query2

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -5,6 +5,8 @@ import styled from "styled-components";
 import Chart from "./Chart";
 import Price from "./Price";
 import {Link} from "react-router-dom";
+import {useQuery} from "react-query";
+import {fetchCoinInfo, fetchCoinTickers} from "../api";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -109,7 +111,7 @@ interface InfoData {
   last_data_at: string;
 }
 
-interface PriceData {
+interface TickersData {
    id: string;
   name: string;
   symbol: string;
@@ -145,62 +147,67 @@ interface PriceData {
 
 
 function Coin() {
-  const [loading, setLoading] = useState(true);
+
   const { coinId } = useParams() as unknown as RouteParams;
   const { state } = useLocation() as RouteState;
-  const [info, setInfo] = useState<InfoData>()
-  const [priceInfo, setPriceInfo] = useState<PriceData>();
   const priceMatch = useMatch("/:coinId/price");
   const chartMarch = useMatch("/:coinId/chart");
 
-  useEffect(() => {
-    (async () => {
-      const infoData = await (
-        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-      ).json();
-      const priceData = await (
-        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-      ).json();
-      setInfo(infoData);
-      setPriceInfo(priceData);
-      setLoading(false);
-    })();
-  }, [coinId]);
+  const {isLoading : infoLoading, data : infoData} = useQuery<InfoData>(["info", coinId], () => fetchCoinInfo(coinId!))
+  const { isLoading : tickersLoading, data : tickersData } = useQuery<TickersData>(["ticker", coinId], () => fetchCoinTickers(coinId!))
+  // const [info, setInfo] = useState<InfoData>()
+  // const [priceInfo, setPriceInfo] = useState<PriceData>();
+  // const [loading, setLoading] = useState(true);
+
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const infoData = await (
+  //       await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+  //     ).json();
+  //     const priceData = await (
+  //       await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+  //     ).json();
+  //     setInfo(infoData);
+  //     setPriceInfo(priceData);
+  //     setLoading(false);
+  //   })();
+  // }, [coinId]);
 
   return (
       <Container>
         <Header>
           <Title>
-            {state?.name ? state.name : loading ? "Loading..." : info?.name}
+            {state?.name ? state.name : infoLoading && tickersLoading ? "Loading..." : infoData?.name}
           </Title>
         </Header>
-        {loading ? (
+        {infoLoading && tickersLoading ? (
             <Loader>Loading...</Loader>
           ) : (
             <>
               <Overview>
                 <OverviewItem>
                   <span>Rank:</span>
-                  <span>{info?.rank}</span>
+                  <span>{infoData?.rank}</span>
                 </OverviewItem>
                 <OverviewItem>
                   <span>Symbol:</span>
-                  <span>${info?.symbol}</span>
+                  <span>${infoData?.symbol}</span>
                 </OverviewItem>
                 <OverviewItem>
                   <span>Open Source:</span>
-                  <span>{info?.open_source ? "Yes" : "No"}</span>
+                  <span>{infoData?.open_source ? "Yes" : "No"}</span>
                 </OverviewItem>
               </Overview>
-              <Description>{info?.description}</Description>
+              <Description>{infoData?.description}</Description>
               <Overview>
                 <OverviewItem>
                   <span>Total Suply:</span>
-                  <span>{priceInfo?.total_supply}</span>
+                  <span>{tickersData?.total_supply}</span>
                 </OverviewItem>
                 <OverviewItem>
                   <span>Max Supply:</span>
-                  <span>{priceInfo?.max_supply}</span>
+                  <span>{tickersData?.max_supply}</span>
                 </OverviewItem>
               </Overview>
 
