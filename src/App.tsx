d498eac95fa8cc93476501908ffe919637ef3833
minheonjun <tamunavollt1@gmail.com>
2023-07-14 11:51:25 +0900

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -70,21 +70,25 @@ a {
 
 const Wrapper = styled(motion.div)`
   display: flex;
-  max-width: 680px;
+  max-width: 100%;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
   align-items: center;
-  height: 1000px;
+  height: 100vh;
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 70px;
+  height: 70px;
   border-radius: 15px;
   background-color: rgba(255, 255, 255, 1);
-  display: grid;
+  display: flex;
+  margin: 5px;
   grid-template-columns: repeat(2, 1fr);
+  justify-content: center;
+  align-items: center;
+  position : absolute;
 `;
 
 const Circle = styled(motion.div)`
@@ -136,17 +140,22 @@ const svg = {
 
 const boxVariants = {
     initial : {
+        x: 500,
         opacity : 0,
         scale : 0
     },
     visible : {
+        transition : {
+            delay : 0.3
+        },
+        x: 0,
         opacity: 1,
         scale : 1,
         rotateZ: 360
     },
-    leaving : {
+    exit : {
+        x : -500,
         opacity : 0,
-        y: 20
     }
 };
 
@@ -154,8 +163,9 @@ const boxVariants = {
 
 function App() {
 
-    const [ showing, setShowing ] = useState(false);
-    const toggleShowing = () => setShowing((prev) => !prev);
+    const [ visible, setVisible ] = useState(1);
+    const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+    const prevPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev - 1));
 
     // Motion Value
     const x = useMotionValue(0);
@@ -171,38 +181,17 @@ function App() {
 
 
     useMotionValueEvent(scrollY, "change", (i) => console.log(i));
-    useMotionValueEvent(x, "change", (i) => console.log(i))
+    useMotionValueEvent( x, "change", (i) => console.log(i))
 
   return (
       <Wrapper style={{ background : gradient}}>
-          {/*<BiggerBox>*/}
-              {/*<button onClick={() => x.set(0)}>HI</button>*/}
-              {/*<Box*/}
-              {/*    style={{ x, rotateZ }}*/}
-              {/*    drag = "x"*/}
-              {/*    dragSnapToOrigin*/}
-              {/*/>*/}
-          {/*</BiggerBox>*/}
-          <button onClick={toggleShowing}>Click</button>
           <AnimatePresence>
-              {showing ? <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving" /> : null }
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+                 item === visible ?  <Box key={item} variants={boxVariants} initial="initial" animate="visible" exit="exit">{item}</Box> : null
+              ))}
           </AnimatePresence>
-          <Svg
-              xmlns="http://www.w3.org/2000/svg"
-              focusable="false"
-              viewBox="0 0 448 512">
-              <motion.path
-                  variants={svg}
-                  initial="start"
-                  animate="end"
-                  transition={{
-                      default: {duration : 5},
-                      fill : {duration: 2 , delay: 3 }
-                  }}
-                  d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-              >
-              </motion.path>
-          </Svg>
+          <button onClick={nextPlease} style={{ marginLeft: 100}}>Next</button>
+          <button onClick={prevPlease} style={{ marginLeft: 100}}>Next</button>
       </Wrapper>
   );
 }
